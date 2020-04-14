ade798eec58cf3db967b787ecbf70244166b3813
Rafael Oliveira <rafa.forsoft.25@gmail.com>
2020-04-14 19:23:09 -0300

    Create two_fer method

--- two-fer/two_fer.rb
+++ two-fer/two_fer.rb
@@ -1,8 +1,9 @@
-=begin
-Write your code for the 'Two Fer' exercise in this file. Make the tests in
-`two_fer_test.rb` pass.
-
-To get started with TDD, see the `README.md` file in your
-`ruby/two-fer` directory.
-=end
-
+class TwoFer
+  def  self.two_fer(name="")
+    if !name.empty?
+      "One for #{name}, one for me."
+    else 
+      "One for you, one for me."
+    end
+  end
+end
