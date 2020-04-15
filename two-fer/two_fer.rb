935920c408a5cdd481bc149e94bc77e837a88bdd
Rafael Oliveira <rafa.forsoft.25@gmail.com>
2020-04-14 21:41:41 -0300

    fix because of feedback in exercism platform

--- two-fer/two_fer.rb
+++ two-fer/two_fer.rb
@@ -1,5 +1,13 @@
 class TwoFer
-  def self.two_fer(name="you")
-    "One for #{name}, one for me."
+  #def self.two_fer(name="you")
+  #  "One for #{name}, one for me."
+  #end
+
+  def self.two_fer(name="")
+    if name.empty?
+      "One for you, one for me."
+    else
+      "One for #{name}, one for me."
+    end
   end
 end
