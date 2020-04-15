e88c2cd6dfec2647d912efc0d64e8cc339e65f20
Rafael Oliveira <rafa.forsoft.25@gmail.com>
2020-04-14 23:00:52 -0300

    resolve exercise rna-transcription in exercism platform

--- rna-transcription/rna_transcription.rb
+++ rna-transcription/rna_transcription.rb
@@ -1,19 +1,11 @@
 class Complement
+  TRANSCRIPTION = { g: 'c', c: 'g', t: 'a', a: 'u' }
+
   def self.of_dna(string_dna)
     return "" if string_dna.empty?
-    
-    string_dna.chars.map do |dna|
-      if dna == "G"
-        "C"
-      elsif dna == "C"
-        "G"
-      elsif dna == "T"
-        "A"
-      elsif dna == "A"
-        "U"
-      else
-        ""
-      end
+
+    string_dna.downcase.chars.map do |dna|
+      TRANSCRIPTION[dna.to_sym].upcase
     end.join
   end
 end
